8a9805ff3174a9e3b73670d43b4a81c8867da9e3
Eldar <wrbeta900@gmail.com>
2025-01-22 12:08:31 +0300

    first_obj_01

---
+++ first_obj.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const book = {
+  title: "War and Peace",
+  author: "Leo Tolstoy",
+  publication_year: 1867,
+};
+console.log(book);
+console.log(book.publication_year);
+console.log(book.title);
